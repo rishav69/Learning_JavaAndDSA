7f0cfecf31d082a43dab90f9c45d624048748c06
rishav69 <risav6973@gmail.com>
2024-10-12 22:44:10 +0530

    newToIt

---
+++ sum.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class sum {
+
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        System.out.print("Enter two numberes:");
+        int a=s.nextInt();
+        int b=s.nextInt();
+        //int c=a+b;
+        System.out.println((a)+"+"+(b)+"=" + (a+b));
+        s.close();
+    }
+}
