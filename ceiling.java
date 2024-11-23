dfbd2029cc1694a42b393e04948b628b49857354
rishav69 <risav6973@gmail.com>
2024-11-23 22:46:33 +0530

    ceiling

---
+++ ceiling.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class ceiling {
+    static int ceil(int a[], int n) {
+        int s = 0, e;
+        e = a.length - 1;
+        while (s <= e) {
+            int mid = s + (e - s) / 2;
+            if (a[mid] == n) {
+                return mid;
+            } else if (a[mid] > n) {
+                e = mid - 1;
+            } else {
+                s = mid + 1;
+            }
+        }
+        return s;                      /*              same code for floor but will return e                 */
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] arr = { 1, 3, 8, 13, 19, 22, 32, 39, 41, 50, 67, 77, 81, 82, 88, 89, 90, 99 };
+        System.out.println("Enter a number to get the ceiling value:");
+        int x = sc.nextInt();
+        int ind=ceil(arr, x);
+        System.out.println("ceilling value is = " + arr[ind]);
+        sc.close();
+    }
+}
